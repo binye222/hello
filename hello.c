41e2feb6847a5386cc2b0c8b52d9e616b18a3ab9
Chris Bourke <cbourke@cse.unl.edu>
2018-08-27 14:24:05 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,13 @@
+/**
+ * Author: Chris Bourke
+ * Date: 2018/01/01
+ *
+ * A basic hello world-style program
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  printf("Chris Bourke\n");
+  return 0;
+}
